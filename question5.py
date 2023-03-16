74785b2e2ad30488f36b2f8326c2694d59607acd
YujieSuCWE <2280914133s@gmail.com>
2023-03-16 14:27:27 -0700

    revise question5

--- question5.py
+++ question5.py
@@ -1,13 +1,13 @@
 # Distributing candy with a while loop
 
-jar_of_candy = 10
-number_of_friends = 0
+number_of_candy = int(input("Please enter the number of candy: \n"))
 
-while jar_of_candy > 0:
+number_of_friends = 0
+while number_of_candy > 0:
     if number_of_friends < 5:
         print("Giving candy to friend", number_of_friends + 1)
-        candy_given = min(jar_of_candy, 3)
-        jar_of_candy -= candy_given
+        candy_given = min(number_of_candy, 3)
+        number_of_candy -= candy_given
         print(candy_given, "pieces of candy given")
         number_of_friends += 1
     else:

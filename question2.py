db3d621819321db14d8c55d4e2d39bd36a92ecde
OwO <gongr@go.oes.edu>
2023-03-16 09:46:39 -0700

    add question3 also update question 2

--- question2.py
+++ question2.py
@@ -4,8 +4,11 @@ import random
 random_number = random.randint(1, 10)
 
 guess = int(input("Guess a number between 1 and 10: "))
-if guess == random_number:
-    print("Yay! you guessed it right!")
-else:
-    print("Sorry, you guessed wrong!")
-    print("The number was", random_number)
+while guess != random_number:
+    if guess < random_number:
+        print("Too low!")
+    else:
+        print("Too high!")
+    guess = int(input("Guess again: "))
+
+print("You win!")

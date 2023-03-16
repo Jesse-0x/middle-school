be791a2c48a2eb961b61be17963f5b3855bc8814
YujieSuCWE <2280914133s@gmail.com>
2023-03-16 13:55:24 -0700

    put question5 to quesion4 and delete question5

--- question4.py
+++ question4.py
@@ -1,2 +1,14 @@
-# a question about for loops
+#Adding numbers from the list
+num1 = int(input("Please enter the first number: \n"))
+num2 = int(input("Please enter the second number: \n"))
+num3 = int(input("Please enter the third number: \n"))
+num4 = int(input("Please enter the fourth number: \n"))
+
+list = [num1, num2, num3, num4]
+
+sum = 0
+for item in list: 
+    sum += item
+
+print ("\nThe sum of the numbers in the list is", sum)
 

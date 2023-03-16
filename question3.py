0e371a2167568460946ad6700cc321bc195eae45
OwO <gongr@go.oes.edu>
2023-03-16 10:01:27 -0700

    add question3

--- question3.py
+++ question3.py
@@ -1,1 +1,11 @@
-# guess the
+# find the password game
+
+# password = "thisisthepassword"
+passwd = [i^0x214 for i in [608, 636, 637, 615, 637, 615, 608, 636, 625, 612, 629, 615, 615, 611, 635, 614, 624]]
+
+password = input("here is my locker, what's my password? ")
+if passwd == password:
+    print("you got it!")
+else:
+    print("wrong password")
+
